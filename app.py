1dacb0d11a46daa1f0c1ba67a746561d0a099458
ed01jaramillo <144198514+ed01jaramillo@users.noreply.github.com>
2024-05-29 18:33:06 -0500

    Update app.py

--- app.py
+++ app.py
@@ -6,14 +6,18 @@ app = Flask(__name__)
 
 # Endpoint para obtener una imagen aleatoria de un gato
 @app.route('/random-cat', methods=['GET'])
+# Endpoint para obtener una imagen aleatoria de un gato
+@app.route('/random-cat', methods=['GET'])
 def get_random_cat():
     try:
         response = requests.get('https://cataas.com/cat')
         response.raise_for_status()  # Verifica si hay errores en la solicitud HTTP
         image_data = BytesIO(response.content)
-        return send_file(image_data, mimetype='image/jpeg')
+        # Crear una respuesta con la imagen y el título
+        return send_file(image_data, mimetype='image/jpeg', as_attachment=True, attachment_filename='imagen_gato.jpg', add_etags=False, cache_timeout=0), "IMAGEN DE GATO RECIBIDA"
     except requests.RequestException as e:
         return jsonify({'error': str(e)}), 500
+
 
 # Endpoint de ejemplo para devolver un valor X
 @app.route('/example', methods=['GET'])
