08dc9af2856d2e77dd1b7690e5f813d942650567
ed01jaramillo <144198514+ed01jaramillo@users.noreply.github.com>
2024-05-29 17:40:48 -0500

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from io import BytesIO
 app = Flask(__name__)
 
 # Endpoint para obtener una imagen aleatoria de un gato
-@app.route('/random-cat', methods=['GET'])
+@app.route('/randomgato', methods=['GET'])
 def get_random_cat():
     try:
         response = requests.get('https://cataas.com/cat')
