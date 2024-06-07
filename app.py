87e96b55ff6de22feeceef068308f64ace3f6222
Ernesto <ernestodani@gmail.com>
2024-06-06 23:20:17 -0500

    Pequeña modificacion en el endpoint de ejemplo

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ def get_random_cat():
 # Endpoint de ejemplo para devolver un valor X
 @app.route('/example', methods=['GET'])
 def example_endpoint():
-    data = {'message': 'Este es un ENDPOINT de ejemplo, si quieres ver fotos de GATOS dirigete al directorio "/random-cat" con la direccion local'}
+    data = {'message': 'Este es una prueba, si quieres ver fotos de GATOS dirigete al directorio "/random-cat" con la direccion local'}
     return jsonify(data)
 
 if __name__ == '__main__':
